14058134f0c841e30afca310bac6602b79854d12
Jasmine Lundgren <jasmine.lundgren@edu.turkuamk.fi>
2020-03-24 11:22:37 +0200

    Marsua klikatessa pomppaa kysymysikkuna

---
+++ MiniPeli/Assets/Scripts/mouseClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseClick : MonoBehaviour
+{
+    public GameObject kyssäIkkuna;
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        kyssäIkkuna.SetActive(true);
+    }
+}
